53dc7ff18d30afbd338cdda3673823f70554f01d
Solange5-sol <usolange195@gmail.com>
2025-10-03 16:10:47 +0200

    Update CheckingAccoun.java

--- CheckingAccoun.java
+++ CheckingAccoun.java
@@ -5,12 +5,12 @@ public class CheckingAccoun extends BankAccount {
     }
     @Override
     public void withdraw(double amount) {
-        if (amount > overdraftLimit) {
+     if (amount >0&& amount<= getBalance()+getOverdraftLimit())
+     {
             setBalance(getBalance() - amount);
             System.out.println("Withdrawing " + amount + " from account " + getAccountNumber());
         }else {
-            System.out.println("limit exceeded");
+            System.out.println("failed");
         }
     }
-
 }

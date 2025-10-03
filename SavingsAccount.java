052c65e56e86f0dae720c8cc2dc40101bb2492c0
Solange5-sol <usolange195@gmail.com>
2025-10-03 16:21:02 +0200

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,15 +1,12 @@
 public class SavingsAccount extends BankAccount {
-    private double interests = 0.50;
     public SavingsAccount(String accountNumber, double balance) {
         super(accountNumber, balance);
     }
-    @Override
-    public void deposit(double amount) {
-        super.deposit(amount);
-    }public void applyInterest() {
-        double interest= getBalance() *interests;
-        setBalance(getBalance()+interest);
-        System.out.println("interests:"+interests);
+    public void applyInterest() {
+        double interestRate = 0.50;
+        double interest = getBalance() * interestRate;
+        setBalance(getBalance() + interest);
+        System.out.println("Interest applied: $" + interest);
     }
-
 }
+
